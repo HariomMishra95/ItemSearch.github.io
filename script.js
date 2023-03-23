648357a070b6ba22c48cd84bff24e73b14e5dc1e
Hariom Mishra <121715030+HariomMishra95@users.noreply.github.com>
2023-03-24 00:28:05 +0530

    ItemSearch

---
+++ script.js
@@ -0,0 +1,19 @@
+function searchb() {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeitem = document.getElementById("product-list");
+    const product = document.querySelectorAll(".product");
+    const pname = document.getElementsByTagName("h4");
+    for (var i = 0; i < pname.length; i++) {
+        let match = product[i].getElementsByTagName('h4')[0];
+        if (match) {
+            let textvalue = match.textContent || match.innerHTML;
+            if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+                product[i].style.display = "";
+            }
+            else {
+                product[i].style.display = "none";
+                product[i].style.innerHTML="sjsj";
+            }
+        }
+    }
+}
